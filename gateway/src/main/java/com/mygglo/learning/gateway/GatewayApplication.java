66114b3a8a91cee480eb326665635d317872d099
James Kokou GAGLO <kokou028666@orange-sonatel.com>
2019-07-01 01:03:50 +0000

    school list and gateway

--- gateway/src/main/java/com/mygglo/learning/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/mygglo/learning/gateway/GatewayApplication.java
@@ -1,14 +1,10 @@
 package com.mygglo.learning.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
